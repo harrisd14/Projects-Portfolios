5b70e961f18e0303c808f9c3fa3956b0f0d13159
harrisd14 <harrisderrick128@gmail.com>
2025-04-03 12:43:41 -0400

    TestUndirectedGraph_Harris.java

--- p5/TestUndirectedGraph.java
+++ p5/TestUndirectedGraph.java
@@ -2,7 +2,7 @@ package p5;
 
 import java.io.IOException;
 
-public class TestUndirectedGraph {
+public class TestUndirectedGraph_HarrisD {
 	
 	public static String squeeze(String s) {
 		if( s == null )
@@ -32,12 +32,9 @@ public class TestUndirectedGraph {
 		else System.out.println("Incorrect toString(); returned "+G1);
 
 		if(G1.countTriplets() == 7)
-			score += 5;
+			score += 10;
 		else System.out.println("Incorrect countTriplets(); returned "+G1.countTriplets());
 
-		if (G1.findShortestPathLengthBetween(0, 8) == 3)
-			score += 10;
-		else System.out.println("Incorrect findShortestPathLengthBetween(); returned "+G1.findShortestPathLengthBetween(0,8));
 
 		if(G1.ifConnectedThenBreadthFirstTraversal()!=null && squeeze(G1.ifConnectedThenBreadthFirstTraversal()).equals(squeeze("0 1 2 5 6 4 8 3 7")))
 			score += 10;
@@ -54,23 +51,19 @@ public class TestUndirectedGraph {
 		score += 10;
 
 		if( squeeze(G2.toString()).equals(squeeze("0: <1,1> <6,2> <7,1> 1: <0,1> <2,1> <3,2> <11,3> 2: <1,1> <3,1> 3: <1,2> <2,1> <4,1> <5,2> 4: <3,1> <5,1> 5: <3,2> <4,1> <6,1> <7,2> 6: <0,2> <5,1> <7,1> 7: <0,1> <5,2> <6,1> 8: <9,4> <10,5> <11,4> 9: <8,4> <10,4> 10: <8,5> <9,4> <11,4> 11: <1,3> <8,4> <10,4>") ))
-			score += 5;
+			score += 10;
 		else System.out.println("Incorrect toString(); returned "+G2);
 
 		if(G2.countTriplets() == 6)
-			score += 5;
-		else System.out.println("Incorrect countTriplets(); returned "+G2.countTriplets());
-		
-		if (G2.findShortestPathLengthBetween(9, 3) == 13)
 			score += 10;
-		else System.out.println("Incorrect findShortestPathLengthBetween(); returned "+G2.findShortestPathLengthBetween(9,3));
+		else System.out.println("Incorrect countTriplets(); returned "+G2.countTriplets());
 
 		if(G2.ifConnectedThenBreadthFirstTraversal()!=null && squeeze(G2.ifConnectedThenBreadthFirstTraversal()).equals(squeeze("0 1 6 7 2 3 11 5 4 8 10 9")))
 			score += 10;
 		else System.out.println("Incorrect ifConnectedThenBreadthFirstTraversal(); returned "+G2.ifConnectedThenBreadthFirstTraversal());
 
 		if(!G2.isTree())
-			score += 5;
+			score += 10;
 		else System.out.println("Incorrect isTree(); returned ");
 		//==========================================================================
 		System.out.println("*******************\nTesting G3 and G4...\n*******************\n");
@@ -78,10 +71,10 @@ public class TestUndirectedGraph {
 		UndirectedGraph G3 = new UndirectedGraph("src/p5/Graphs/p5-G3.txt");
 		UndirectedGraph G4 = new UndirectedGraph("src/p5/Graphs/p5-G4.txt");
 
-		score += 5;
+
 
 		if(G3.isTree() && !G4.isTree())
-			score += 5;
+			score += 10;
 		else System.out.println("Incorrect isTree(); returned ");
 		//==========================================================================
 		System.out.println("Your score: " + score + "/100");
